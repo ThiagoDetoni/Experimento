025ead389b980c18fdad40c09dc5c9556273b330
ThiagoDetoni <thiago@xbrain.com.br>
2017-04-08 09:52:19 -0300

    Add files via upload
    retorno boleano para metodo salvar

--- src/main/java/bean/Services.java
+++ src/main/java/bean/Services.java
@@ -54,14 +54,18 @@ public class Services {
         lst = new controller.ServicesController().getAll();
     }
     
-    public void save(){
+    public boolean save(){
         model.Services s = new model.Services();
         s.setName(name);
         Users u = new Users();
         u.setId(idUser);
         s.setIdUser(u);
-        
-        new controller.ServicesController().persist(s);
+        try{
+            new controller.ServicesController().persist(s);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
     }
     
 }
